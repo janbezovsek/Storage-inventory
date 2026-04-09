6f4b57b055e8b3fcebca1d769d2679a1e82a0fc5
alfa1omega <bezovsekjan@gmail.com>
2026-04-09 20:02:18 +0200

    console.log removed

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export async function login(req, res) {
-  console.log('Login attempt:', req.body)
+  
   const { username, password } = req.body
 
   if (!username || !password) {
